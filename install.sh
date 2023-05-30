b69aaaa458936b3872dbc5490c100bf857570d88
Sid Kothari <sid.kothari7@gmail.com>
2023-05-30 07:51:03 -0400

    Fix mkdirs in install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,20 @@
 
 files=(.bash_aliases .bash_profile .bashrc .gitconfig .gitignore_global .inputrc .vimrc .vim)
 
-OLDDOTFILES=~/.olddotfiles
-DOTFILES=~/dotfiles
+OLDDOTFILES=$HOME/.olddotfiles
+DOTFILES=$HOME/dotfiles
+
+mkdir OLDDOTFILES && mkdir DOTFILES
+
 # move any existing dotfiles in homedir to dotfiles_old directory, then create
 # symlinks
-echo -e "Moving any existing dotfiles from ~ to $OLDDOTFILES & creating symlinks...\n"
-for file in $files; do
-    if [ -e ~/$file ]; then
-	mv -v ~/$file $OLDDOTFILES/
+echo -e "Moving any existing dotfiles from $HOME to $OLDDOTFILES & creating symlinks...\n"
+for file in "${files[@]}"; do
+    if [[ -e $HOME/$file ]]; then
+	mv -v $HOME/$file $OLDDOTFILES/$file
     fi
-    ln -snfv $DOTFILES/$file ~/$file
+    ln -snfv $DOTFILES/$file $HOME/$file
 done
 
-echo -e "\nSourcing ~/.bash_profile\n"
-. ~/.bash_profile
+echo -e "\nSourcing $HOME/.bash_profile\n"
+. $HOME/.bash_profile
