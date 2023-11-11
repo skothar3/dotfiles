0272894a1baf5abe2858a5676be4e087cf3ed9f9
Sid Kothari <sid.kothari7@gmail.com>
2023-11-11 16:17:19 -0500

    Quote shell vars, update pkg installation to include Fedora, move vim PlugInstall to end of script

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -o nounset
 set -o pipefail
 
 # Get sudo password for Linux package manager commands
-[[ "$(uname -v)" =~ Ubuntu ]] && read -sp 'Enter the sudo password: ' PW
+[[ "$(uname -v)" =~ Ubuntu|Fedora ]] && read -sp 'Enter the sudo password: ' PW
 
 # Array of dot files to source
 files=(.bash_aliases .bash_profile .bashrc .gitconfig .gitignore_global .inputrc .vimrc .vim)
@@ -21,24 +21,29 @@ echo -e "\nChecking paths to dotfiles...\n"
 # Get the parent directory of install.sh
 curr_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [[ -e $DOTFILES ]]; then
-    # If the current directory is NOT already $DOTFILES, then clear $DOTFILES and move everything there
-    if ! [[ $curr_dir -ef $DOTFILES ]]; then
-        rm -rfv $DOTFILES
-        mv -v $curr_dir $DOTFILES
+if [[ -e "$DOTFILES" ]]; then
+
+    # If the current directory is NOT already "$DOTFILES", then clear "$DOTFILES" and move everything there
+    if ! [[ "$curr_dir" -ef "$DOTFILES" ]]; then
+        rm -rfv "$DOTFILES"
+        mv -v "$curr_dir" "$DOTFILES"
     fi
 else
-    # Make $DOTFILES and move everything there
-    mv -v $curr_dir $DOTFILES
+    # Make "$DOTFILES" and move everything there
+    mv -v "$curr_dir" "$DOTFILES"
+
 fi
 
-# If $OLD_DOTFILES doesn't already exist, then create it
-[[ -d $OLD_DOTFILES ]] || mkdir -v $OLD_DOTFILES
+# If "$OLD_DOTFILES" doesn't already exist, then create it
+[[ -d "$OLD_DOTFILES" ]] || mkdir -v "$OLD_DOTFILES"
+
 echo -e "Done...\n"
 
-# Move any existing dotfiles in $HOME to $OLD_DOTFILES, then create new symlinks from $HOME to $DOTFILES
+# Move any existing dotfiles in $HOME to "$OLD_DOTFILES", then create new symlinks from $HOME to "$DOTFILES"
 echo -e "Moving any existing dotfiles from $HOME to $OLD_DOTFILES & creating symlinks to $DOTFILES...\n"
+
 for file in "${files[@]}"; do
+
     # If a same-named file already exists in $HOME:
     # 1. Remove it, if it is a symlink or
     # 2. Move it, if it's anything else
@@ -49,6 +54,7 @@ for file in "${files[@]}"; do
     fi
     # Create a new symlink at $HOME to $file
     ln -snfv "$DOTFILES/$file" "$HOME/$file"
+
 done
 echo -e "Done...\n"
 
@@ -60,30 +66,39 @@ echo -e "Configuring .ssh/ for future logins...\n"
 
 # Check for my public key
 if ! grep -wq "sko" "$HOME/.ssh/authorized_keys"; then
+
     echo -e "Attempting to add public key...\n"
+
     # Add my public key to trust future remote logins
     if [[ -f "$DOTFILES/id_rsa.pub" ]]; then
         cat "$DOTFILES/id_rsa.pub" >>"$HOME/.ssh/authorized_keys"
     fi
+
     echo -e "Done...\n"
+
 fi
 
 # Install necessary packages
-if [[ "$(uname -v)" =~ Ubuntu ]]; then
-    pkgs=(ripgrep bat fzf fd-find)
-    echo -e "Installing packages:\n"
-    printf '%s\n' "${pkgs[@]}"
-
-    # Passes $PW obtained at start into each sudo call
-    echo "$PW" | sudo apt -y update
-    echo "$PW" | sudo apt -y upgrade
-    echo "$PW" | sudo apt install "${pkgs[@]}"
-    echo -e "Done...\n"
+if [[ "$(uname -v)" =~ "Ubuntu" ]]; then
+    pkgman="apt"
+elif [[ "$(uname -v)" =~ "Fedora" ]]; then
+    pkgman="dnf"
+elif [[ "$(uname -v)" != "Darwin" ]]; then
+    pkgman="brew"
 fi
 
+pkgs=(ripgrep bat fzf fd-find)
+echo -e "Installing packages:\n"
+printf '%s\n' "${pkgs[@]}"
+
+# Passes $PW obtained at start into each sudo call
+echo "$PW" | sudo "$pkgman" -y update
+echo "$PW" | sudo "$pkgman" -y upgrade
+echo "$PW" | sudo "$pkgman" install "${pkgs[@]}"
+
+echo -e "Done...\n"
+
 echo -e "Installing vim plugins...\n"
-# Vim ex mode and silent mode, load my .vimrc, execute PlugInstall from vim-plug, and then quit
-vim -es -u "$HOME/.vimrc" -i NONE -c "PlugInstall" -c "qa"
 
 # Symlink vim colorscheme after gruvbox plugin is installed
 echo -e "Linking gruvbox colorscheme...\n"
@@ -92,5 +107,8 @@ echo -e "Done...\n"
 
 echo -e "\nSourcing $HOME/.bash_profile...\n"
 . "$HOME/.bash_profile"
+
+# Vim ex mode and silent mode, load my .vimrc, execute PlugInstall from vim-plug, and then quit
+vim -es -u "$HOME/.vimrc" -i NONE -c "PlugInstall" -c "qa"
 
 echo -e "Finished!\n"
